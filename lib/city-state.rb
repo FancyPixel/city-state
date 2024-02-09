333f2bb16e6e5d99fdda3fb1568540b095e9b687
Caterina Meneghini <caterina.meneghini@fancypixel.it>
2024-02-09 17:29:35 +0100

    Add prov_state method

--- lib/city-state.rb
+++ lib/city-state.rb
@@ -11,9 +11,10 @@ module CS
   DEFAULT_STATES_LOOKUP_FN    = 'db/states-lookup.yml'
   DEFAULT_PROVINCES_LOOKUP_FN    = 'db/provinces-lookup.yml'
   DEFAULT_COUNTRIES_LOOKUP_FN = 'db/countries-lookup.yml'
+  DEFAULT_PROV_STATE_LOOKUP_FN = 'db/prov_state-lookup.yml'
   ITALIAN_CITIES_FN = File.join(FILES_FOLDER, "Elenco-comuni-italiani.csv")
 
-  @countries, @states, @cities, @cities_prov, @provinces = [{}, {}, {}, {}, {}]
+  @countries, @states, @cities, @cities_prov, @provinces, @prov_state = [{}, {}, {}, {}, {}, {}]
   @current_country = nil # :US, :BR, :GB, :JP, ...
   @maxmind_zip_url = nil
   @license_key = nil
@@ -27,6 +28,7 @@ module CS
   @provinces_lookup = nil
   @countries_lookup_fn = nil
   @countries_lookup = nil
+  @prov_state_lookup = nil
 
   def self.set_maxmind_zip_url(maxmind_zip_url)
     @maxmind_zip_url = maxmind_zip_url
@@ -101,6 +103,7 @@ module CS
     cities = {}
     provinces = {}
     cities_prov = {}
+    prov_state = {}
     states = {}
     File.foreach(MAXMIND_DB_FN) do |line|
       rec = line.split(",")
@@ -131,6 +134,10 @@ module CS
         provinces.merge!(province)
       end
 
+      # provinces list: {45: [:"FE", :"BO", ... ]} with dups
+      prov_state.merge!({rec[STATE] => []}) if ! states.has_key?(rec[STATE])
+      prov_state[rec[STATE]] << rec[PROV]
+
       # cities list: {TX: ["Texas City", "Another", "Another 2"]}
       cities.merge!({rec[STATE] => []}) if ! states.has_key?(rec[STATE])
       cities[rec[STATE]] << rec[CITY]
@@ -156,17 +163,22 @@ module CS
       # sort provinces and cities_prov
       cities_prov = Hash[cities_prov.sort]
       provinces = Hash[provinces.sort]
+      prov_state = Hash[prov_state.sort]
+      p prov_state
       unless country == "IT"
         cities_prov.each { |k, v| cities_prov[k].sort! }
       else
         cities_prov.each { |k, v| cities_prov[k] = cities_prov[k].invert.sort.to_h.invert }
+        prov_state.each { |k, v| prov_state[k] = prov_state[k].uniq!.sort }
       end
 
       provices_fn = File.join(FILES_FOLDER, "provinces.#{country.downcase}")
       cities_prov_fn = File.join(FILES_FOLDER, "cities_prov.#{country.downcase}")
+      prov_state_fn = File.join(FILES_FOLDER, "prov_state.#{country.downcase}")
       File.open(provices_fn, "w") { |f| f.write provinces.to_yaml }
       File.open(cities_prov_fn, "w") { |f| f.write cities_prov.to_yaml }
-      File.chmod(0666, provices_fn, cities_prov_fn) # force permissions to rw_rw_rw_ (issue #3)
+      File.open(prov_state_fn, "w") { |f| f.write prov_state.to_yaml }
+      File.chmod(0666, provices_fn, cities_prov_fn, prov_state_fn) # force permissions to rw_rw_rw_ (issue #3)
     end
 
     # sort
@@ -195,7 +207,7 @@ module CS
       @current_country = :US
       self.install(@current_country)
 
-      # we find a file: normalize the extension to something like :US
+    # we find a file: normalize the extension to something like :US
     else
       @current_country = @current_country.to_s.upcase.to_sym
     end
@@ -215,7 +227,7 @@ module CS
     # load the country file
     if self.blank?(@cities[country])
       cities_fn = File.join(FILES_FOLDER, "cities.#{country.to_s.downcase}")
-      self.install(country) unless File.exist?(cities_fn)
+      self.install(country) if ! File.exist? cities_fn
       @cities[country] = self.symbolize_keys(YAML::load_file(cities_fn))
 
       # Remove duplicated cities
@@ -224,23 +236,21 @@ module CS
       end
 
       # Process lookup table
-      lookup = get_cities_lookup(country)
-      unless lookup.nil?
-        lookup.each do |state, replacements|
-          replacements.each do |old_value, new_value|
-            if new_value.nil? || self.blank?(new_value)
-              @cities[country][state].delete(old_value)
+      lookup = get_cities_lookup(country, state)
+      if ! lookup.nil?
+        lookup.each do |old_value, new_value|
+          if new_value.nil? || self.blank?(new_value)
+            @cities[country][state].delete(old_value)
+          else
+            index = @cities[country][state].index(old_value)
+            if index.nil?
+              @cities[country][state][] = new_value
             else
-              index = @cities[country][state].index(old_value)
-              if index.nil?
-                @cities[country][state] << new_value
-              else
-                @cities[country][state][index] = new_value
-              end
+              @cities[country][state][index] = new_value
             end
           end
-          @cities[country][state] = @cities[country][state].sort # sort it alphabetically
         end
+        @cities[country][state] = @cities[country][state].sort # sort it alphabetically
       end
     end
 
@@ -277,7 +287,7 @@ module CS
     @countries_lookup    = nil
   end
 
-  def self.get_cities_lookup(country)
+  def self.get_cities_lookup(country, state)
     # lookup file not loaded
     if @cities_lookup.nil?
       @cities_lookup_fn  = DEFAULT_CITIES_LOOKUP_FN if @cities_lookup_fn.nil?
@@ -287,8 +297,22 @@ module CS
       @cities_lookup.each { |key, value| @cities_lookup[key] = self.symbolize_keys(value) } # force states to be symbols
     end
 
-    return nil unless @cities_lookup.key?(country)
-    @cities_lookup[country]
+    return nil if ! @cities_lookup.key?(country) || ! @cities_lookup[country].key?(state)
+    @cities_lookup[country][state]
+  end
+
+  def self.get_prov_state_lookup(state)
+    # lookup file not loaded
+    if @prov_state_lookup.nil?
+      @prov_state_lookup_fn  = DEFAULT_PROV_STATE_LOOKUP_FN if @prov_state_lookup_fn.nil?
+      @prov_state_lookup_fn  = File.expand_path(@prov_state_lookup_fn)
+      return nil if ! File.exist?(@prov_state_lookup_fn)
+      @prov_state_lookup = self.symbolize_keys(YAML::load_file(@prov_state_lookup_fn)) # force countries to be symbols
+      @prov_state_lookup.each { |key, value| @prov_state_lookup[key] = self.symbolize_keys(value) } # force states to be symbols
+    end
+
+    return nil if ! @prov_state_lookup.key?(state)
+    @prov_state_lookup[state]
   end
 
   def self.get_states_lookup(country)
@@ -329,6 +353,39 @@ module CS
     end
 
     @countries_lookup
+  end
+
+  def self.prov_state(state, country)
+    # Bugfix: https://github.com/loureirorg/city-state/issues/24
+    return {} if (country.nil? || country != "IT".to_sym) 
+
+    # Set it as current_country
+    self.current_country = country # set as current_country
+    country = self.current_country # normalized
+
+    # Load the country file
+    if self.blank?(@prov_state[state])
+      prov_state_fn = File.join(FILES_FOLDER, "prov_state.#{country.to_s.downcase}")
+      self.install(country) if ! File.exist? prov_state_fn
+      @prov_state = self.symbolize_keys(YAML::load_file(prov_state_fn))
+
+      # Process lookup table
+      lookup = get_prov_state_lookup(state)
+      if ! lookup.nil?
+        lookup.each do |key, value|
+          if value.nil? || self.blank?(value)
+            @prov_state[state].delete(key)
+          else
+            @prov_state[state][key] = value
+          end
+        end
+        @prov_state[state] = @prov_state[state].sort.to_h # sort it alphabetically
+      end
+    end
+
+    # p @prov_state
+    # Return list
+    @prov_state[state] || {}
   end
 
   def self.provinces(country)
